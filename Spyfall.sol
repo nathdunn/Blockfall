6cc9f6d153162400d6da2a8b0e47b1adf277cdae
nathdunn <34898747+nathdunn@users.noreply.github.com>
2019-04-17 22:53:27 -0600

    Update Spyfall.sol

--- Spyfall.sol
+++ Spyfall.sol
@@ -5,12 +5,44 @@ contract Spyfall {
 
     // Remember: simple variables can be viewed with a CALL without having to define a getter function
     string[] public players;
-    string public name;
+    string public questioner;
     string private spy;
     string private location;
+    string[] public locations = {'Autoshop', 'Gas Station', 'Police Station', 'Fire Station', 'Film Studio', 'Beach'}
+    
+    function nameSpy(string[] players) private{
+           //pickspy
+           spy = player[i]
+    }
+    
+    function nameQuestioner(string[] players) public{
+           //pick questioner
+           questioner = player[i]
+    }
+    
+    function pickLocation(string[] locations) public{
     
     
+    }
+    
+    function sendQuestion(sender, recipient){
+        //is sender=recipient?
+            //if so, accept question
+        //set receiver as new recipient
+    }
+    
+    function answerQuestion(recipient){
+        //answer question
+    
+    }
 
+    
+    function putForthGuessOfWhoSpyIs(){
+    
+    }
 
+    function putForthGuessOfWhatPlaceIs() //requirement - must be spy{
+    
+    }
 
 }
